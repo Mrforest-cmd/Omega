19326dfc32cfeac226c9106fac407718b41fd840
Mr.forest <lehan020809@gmail.com>
2023-05-18 19:44:47 +0300

    Update Pro-c.cpp
    За базу беру проект с урока

--- Pro-c/Pro-c.cpp
+++ Pro-c/Pro-c.cpp
@@ -1,10 +1,66 @@
-﻿// Pro-c.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include<Windows.h>
+#include<iostream>
+#include <stdlib.h>
 
-#include <iostream>
+
+
 using namespace std;
-int main()
+
+
+
+bool KEY[256];
+void GetKEY()
 {
-    cout << "Hello World!\n";
+	int i = 0;
+	while (i < 256)
+	{
+		if (GetAsyncKeyState(i)) {
+			KEY[i] = 1;
+		}
+		else {
+			KEY[i] = 0;
+		}
+		i++;
+	}
 }
 
+
+
+
+void main() {
+	char p = 219;
+	char b = 8;
+	int d = 0;
+	string outPut = "";
+	while (true) {
+		GetKEY();
+		if (KEY[39])
+		{
+			outPut += " ";
+			d++;
+		}
+		else if (KEY[37])
+		{
+			if (size(outPut) > 0) {
+				outPut += b;
+				d--;
+			}
+		}
+		else if (KEY[40])
+		{
+			outPut += "\n";
+			for (int i = 0; i < d; i++) {
+				outPut += " ";
+			}
+
+		}
+		else if (KEY[38])
+		{
+			outPut += "";
+			outPut += "\033[A";
+		}
+		cout << "\033[2J\033[1;1H";
+		cout << outPut << p << endl;
+		Sleep(200);
+	}
+}

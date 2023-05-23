ea8a4b128aea15296bf6fb9f9057f0bca65d504a
Mr.forest <lehan020809@gmail.com>
2023-05-23 20:22:51 +0300

    test

--- Pro-c/Pro-c.cpp
+++ Pro-c/Pro-c.cpp
@@ -2,18 +2,14 @@
 #include<iostream>
 #include <stdlib.h>
 
-
-
 using namespace std;
-
-
 
 bool KEY[256];
 void GetKEY()
 {
 	int i = 0;
 	while (i < 256)
-	{
+	{	
 		if (GetAsyncKeyState(i)) {
 			KEY[i] = 1;
 		}
@@ -24,10 +20,8 @@ void GetKEY()
 	}
 }
 
-
-
-
 void main() {
+	char b = 219;
 	char p = 219;
 	char b = 8;
 	int d = 0;

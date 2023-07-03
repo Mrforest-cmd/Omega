4781e862536d698f919d961e91d4c75e98fd33bf
Mr.forest <123559041+Mrforest-cmd@users.noreply.github.com>
2023-07-03 19:40:47 +0300

    Реализация Основы
    Я изменил концепцию, теперь мы используем библиотеку conio (думаю так будет удобнее).
    Функции:
    start - Установка начальных значений.
    game - пока-что только движение змейки.
    sketch - отрисовка карты.
    Удачки в постройке своего кода, вопросы в лс

--- Pro-c/Pro-c.cpp
+++ Pro-c/Pro-c.cpp
@@ -1,32 +1,95 @@
-﻿#include<Windows.h>
-#include<iostream>
-#include <stdlib.h>
-#include <string>
+#include <iostream>
+#include <conio.h>
+
 using namespace std;
 
-bool KEY[256];
-void GetKEY()
-{
-	int i = 0;
-	while (i < 256)
-	{	
-		if (GetAsyncKeyState(i)) {
-			KEY[i] = 1;
+bool isdead;
+int pos_x, pos_y, score, pos_fruit_x, pos_fruit_y;
+const int map_width = 20;
+const int map_height = 20;
+
+enum direction {RIGHT,LEFT,UP,DOWN,STOP=1};
+direction directi;
+
+void start() {
+	isdead = false;
+	directi = STOP;
+	pos_x = map_width / 2 - 1;
+	pos_y = map_height / 2 - 1;
+	pos_fruit_x = rand() % map_width;
+}
+
+void game() {
+	switch (directi) {
+	case RIGHT:
+		pos_x++;
+		break;
+	case LEFT:
+		pos_x--;
+		break;
+
+	case UP:
+		pos_y--;
+		break;
+
+	case DOWN:
+		pos_y++;
+		break;
+
+	}
+}
+	
+void control() {
+	if (_kbhit) {
+		switch (_getch()) {
+		case 'a':
+			directi = LEFT;
+			break;
+		case 'd':
+			directi = RIGHT;
+			break;
+		case 'w':
+			directi = UP;
+			break;
+		case 's':
+			directi = DOWN;
+			break;
 		}
-		else {
-			KEY[i] = 0;
+		if (pos_x > map_width || pos_y > map_height || pos_x < 0 || pos_y < 0) {
+			isdead = true;
 		}
-		i++;
 	}
 }
 
-void main() {
-	char b = 219;
-	char b = 8;
-	const int x = 10;
-	const int y = 10;
-	string map[x + 1][y + 1];
-	for (int i = 0; i < y + 1; i++) {
-		cout << "-";
+void sketch() {
+	system("cls");
+	for (int i = 0; i < map_width + 1; i++) {
+		cout << "#";	}
+	cout << endl;
+
+	for (int i = 0; i < map_height; i++) {
+		for (int w = 0; w < map_width; w++) {
+			if (w == map_width - 1 || w == 0) {
+				cout << "#";
+			}
+			if (i == pos_y && w == pos_y) {
+				cout << "s";
+			}
+			else if (w == pos_fruit_x && i == pos_fruit_y) {
+				cout << "*";
+			}
+			else cout << " ";
+		}
 	}
+
+	for (int i = 0; i < map_width + 1; i++) {
+		cout << "#";
+	}
+	cout << endl;
+}
+
+
+
+void main() {
+	srand((unsigned)time(NULL));
 }

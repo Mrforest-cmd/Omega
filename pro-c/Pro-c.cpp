464157334907bf5f7cf41eca560229df8fc9384c
Mr.forest <123559041+Mrforest-cmd@users.noreply.github.com>
2023-07-05 21:27:02 +0300

    Конец

--- pro-c/Pro-c.cpp
+++ pro-c/Pro-c.cpp
@@ -13,18 +13,19 @@ int pos_hvost_x[50], pos_hvost_y[50], dlinna_hvostika;
 
 enum direction { RIGHT, LEFT, UP, DOWN, STOP = 1 };
 direction directi;
-
+// Установка начальных значений
 void start() {
     score = 0;
     isdead = false;
     directi = STOP;
     pos_x = map_width / 2;
     pos_y = map_height / 2;
-    pos_fruit_x = (rand() % (map_width - 2)) + 1;
-    pos_fruit_y = (rand() % (map_height - 2)) + 1;
+    pos_fruit_x = rand() % (map_width - 2) + 1;
+    pos_fruit_y = rand() % (map_height - 2) + 1;
 }
 
 void game() {
+    // Тут делаеться хвост.
     int temp_save_x = pos_hvost_x[0], temp_save_y = pos_hvost_y[0], temp1, temp2;
     pos_hvost_x[0] = pos_x, pos_hvost_y[0] = pos_y;
     for (int i = 1; i < dlinna_hvostika; i++) {
@@ -35,7 +36,7 @@ void game() {
         temp_save_x = temp1;
         temp_save_y = temp2;
     }
-
+    // Тут проверка на значение directi (управление)
     switch (directi) {
     case RIGHT:
         pos_x++;
@@ -52,7 +53,7 @@ void game() {
         pos_y++;
         break;
     }
-
+    // Тут проверка на удар об стену
     if (pos_x > map_width - 2 || pos_y > map_height - 1 || pos_x < 0 || pos_y < 0) {
         isdead = true;
         for (int i = 0; i < dlinna_hvostika; i++) {
@@ -61,23 +62,25 @@ void game() {
             }
         }
     }
-
+    // Тут проверка на удар об звост
     for (int i = 1; i < dlinna_hvostika; i++) {
         if (pos_x == pos_hvost_x[i] && pos_y == pos_hvost_y[i]) {
             isdead = true;
             return;
         }
     }
-
+    // Тут зачисление очков + удлиннение хвоста
     if (pos_x == pos_fruit_x && pos_y == pos_fruit_y) {
         dlinna_hvostika++;
         score++;
+        // И ещё регенерация фруктов) (- 2 - чтобы не выйти за границы карты и -1 для того,чтобы они появлялись на последнем столбике перед правой стеной)
         pos_fruit_x = (rand() % (map_width - 2)) + 1;
         pos_fruit_y = (rand() % (map_height - 2)) + 1;
     }
 }
-
+// Нажатия клавишь (игра работает только с английской раскладкой)
 void controls() {
+    // Реализовано оно так, чтобы мы не могли пойти сразу в противоположную сторону, что-бы не врезаться в самого себя
     if (_kbhit()) {
         switch (_getch()) {
         case 'a':
@@ -101,33 +104,39 @@ void controls() {
         }
     }
 }
-
+// Отрисовка карты
 void sketchup() {
     system("cls");
+    // потолок
     for (int i = 0; i < map_width + 1; i++) {
-        cout << "#";
+        cout << "_";
     }
     cout << endl;
 
     for (int i = 0; i < map_height; i++) {
         for (int w = 0; w < map_width; w++) {
+            // стены
             if (w == 0 || w == map_width - 1) {
-                cout << "#";
+                cout << "|";
             }
+            // голова змеи
             if (i == pos_y && w == pos_x) {
-                cout << "0";
+                cout << "*";
             }
+            // фрукт
             else if (w == pos_fruit_x && i == pos_fruit_y) {
-                cout << "F";
+                cout << "#";
             }
             else {
                 bool nospace = false;
+                // хвост
                 for (int b = 0; b < dlinna_hvostika; b++) {
                     if (pos_hvost_y[b] == i && pos_hvost_x[b] == w) {
-                        cout << "0";
+                        cout << "*";
                         nospace = true;
                     }
                 }
+                // пустое место на карте
                 if (!nospace) {
                     cout << " ";
                 }
@@ -135,11 +144,11 @@ void sketchup() {
         }
         cout << endl;
     }
-
+    // пол
     for (int i = 0; i < map_width + 1; i++) {
-        cout << "#";
+        cout << "_";
     }
-    cout << endl;
+    cout << endl << endl;
     cout << "Score: " << score;
 }
 
@@ -151,6 +160,6 @@ int main()
         sketchup();
         controls();
         game();
-        Sleep(150);
+        Sleep(100);
     }
 }
